41de40d23546b20a1031c97941a152bc52c9b4b6
TimShiker <timpalshi@gmail.com>
2022-11-06 23:39:53 +0200

    rename controller of post and add to this controller new annotations

--- src/main/java/telran/java2022/post/controller/PostController.java
+++ src/main/java/telran/java2022/post/controller/PostController.java
@@ -3,12 +3,15 @@ package telran.java2022.post.controller;
 import java.util.HashSet;
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.RequiredArgsConstructor;
@@ -21,51 +24,53 @@ import telran.java2022.post.service.ForumService;
 
 @RestController
 @RequiredArgsConstructor
-public class ForumController {
+@RequestMapping("/forum")
+public class PostController {
 	
 	final ForumService forumService;
 	
-	@PostMapping("/forum/post/{author}")
+	@PostMapping("/post/{author}")
 	public PostDto addPost(@PathVariable String author, @RequestBody PostCreateDto postCreateDto) {
 		return forumService.addPost(author, postCreateDto);
 	}
 	
-	@GetMapping("/forum/post/{id}")
+	@GetMapping("/post/{id}")
 	public PostDto findPost(@PathVariable String id) {
 		return forumService.findPost(id);
 	}
 	
-	@PutMapping("/forum/post/{id}/like")
+	@PutMapping("/post/{id}/like")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
 	public void addLike(@PathVariable String id) {
 		forumService.addLike(id);
 	}
 	
-	@GetMapping("/forum/posts/author/{author}")
+	@GetMapping("/posts/author/{author}")
 	public List<PostDto> findPostsByAuthor(@PathVariable String author) {
 		return forumService.getPostsByAuthor(author);
 	}
 	
-	@PutMapping("/forum/post/{id}/comment/{user}")
+	@PutMapping("/post/{id}/comment/{user}")
 	public PostDto addComment(@PathVariable String id, @PathVariable String user, @RequestBody CommentDto commentDto) {
 		return forumService.addComment(id, user, commentDto);
 	}
 	
-	@DeleteMapping("/forum/post/{id}")
+	@DeleteMapping("/post/{id}")
 	public PostDto deletePost(@PathVariable String id) {
 		return forumService.deletePost(id);
 	}
 	
-	@PostMapping("/forum/posts/tags")
+	@PostMapping("/posts/tags")
 	public List<PostDto> findPostByTags(@RequestBody HashSet<String> tags) {
 		return forumService.getPostsByTags(tags);
 	}
 	
-	@PostMapping("/forum/posts/period")
+	@PostMapping("/posts/period")
 	public List<PostDto> findPostByPeriod(@RequestBody PostGetByPeriodDto postGetByPeriodDto) {
 		return forumService.getPostsByPeriod(postGetByPeriodDto);
 	}
 	
-	@PutMapping("/forum/post/{id}")
+	@PutMapping("/post/{id}")
 	public PostDto updatePost(@PathVariable String id, @RequestBody PostUpdateDto postUpdateDto) {
 		return forumService.updatePost(id, postUpdateDto);
 	}
